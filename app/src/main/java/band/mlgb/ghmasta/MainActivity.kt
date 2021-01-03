a017521ceaa96ab0c4280443fa9fee93468d9ca3
Chen Cen <arrow.cen@gmail.com>
2021-01-02 17:41:07 -0800

    init

---
+++ app/src/main/java/band/mlgb/ghmasta/MainActivity.kt
@@ -0,0 +1,59 @@
+package band.mlgb.ghmasta
+
+import android.app.SearchManager
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.widget.SearchView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(setOf(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        handleIntent(intent)
+    }
+    private fun handleIntent(intent: Intent) {
+
+        if (Intent.ACTION_SEARCH == intent.action) {
+            val query = intent.getStringExtra(SearchManager.QUERY)
+            //use the query to search your data somehow
+            query?.let { Log.d("BGLM", "searched $it") } ?: run {
+                Log.d("BGLM", "searched nothing")
+            }
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.search_menu, menu)
+        (getSystemService(Context.SEARCH_SERVICE) as SearchManager).let { searchManager ->
+            (menu?.findItem(R.id.search)?.actionView as SearchView).apply {
+                setSearchableInfo(searchManager.getSearchableInfo(componentName))
+            }
+        }
+        return true
+    }
+
+}
---
+++ app/src/main/java/band/mlgb/ghmasta/ui/data/Repository.kt
@@ -0,0 +1,9 @@
+package band.mlgb.ghmasta.ui.data
+
+data class Repository(
+
+    val id: Long,
+    val description: String,
+
+    )
+
