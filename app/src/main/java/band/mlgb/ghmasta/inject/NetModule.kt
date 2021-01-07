d4fe0e91bd9fee481eaa78bba8a606d7b18f01e7
Chen Cen <arrow.cen@gmail.com>
2021-01-07 00:52:54 -0800

    add auth bearer

--- app/src/main/java/band/mlgb/ghmasta/inject/NetModule.kt
+++ app/src/main/java/band/mlgb/ghmasta/inject/NetModule.kt
@@ -5,6 +5,7 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ApplicationComponent
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
@@ -12,13 +13,33 @@ import javax.inject.Singleton
 @Module
 @InstallIn(ApplicationComponent::class)
 class NetModule {
+
+    // check balance:
+    // curl -H "Authorization: Bearer 1147bd6dcbf1eb6658ec115a0eb543d7c32e4d43" https://api.github.com/rate_limit
     @Provides
     @Singleton
-    fun provideGithubApi(): GithubApi {
+    fun provideOKHttpClientWithAuthBearer(): OkHttpClient {
+        return OkHttpClient.Builder().addInterceptor { chain ->
+            chain.proceed(
+                chain.request().newBuilder().addHeader(
+                    "Authorization",
+                    "Bearer $ACCESS_TOKEN"
+                ).build()
+            )
+        }.build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideGithubApi(okHttpClient: OkHttpClient): GithubApi {
         return Retrofit.Builder()
+            .client(okHttpClient)
             .baseUrl("https://api.github.com/")
             .addConverterFactory(GsonConverterFactory.create())
             .build().create(GithubApi::class.java)
     }
 
+    companion object {
+        private const val ACCESS_TOKEN = "1147bd6dcbf1eb6658ec115a0eb543d7c32e4d43"
+    }
 }
